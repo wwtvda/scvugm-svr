cb8c694f26c5ee21e547cb8a03a6112edcd16568
wwtvda <mindiehashburry@gmail.com>
2022-01-17 15:40:04 +0700

    Method delete

--- src/routes/isola.rte.js
+++ src/routes/isola.rte.js
@@ -1,29 +1,20 @@
 var express = require('express');
 const { send } = require('express/lib/response');
+const { findOne } = require('../schema/isola.js');
 var router = express.Router();
 const app = express()
 const port = 4000
 
-
-
 const Isola = require('../schema/isola.js');
 
-
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 router.get('/isola', async function (req, res) {
   const data = await Isola.find();
   res.json(data);
 })
 
-/**
- * req.body =
- * {
- *   lokasi: 'baciro selatan',
- *   masuk: '2022/10/11',
- *   keluar: '2022/10/20',
- *   status: 'Selesai Isolasi'
- * }
- */
 
 router.post('/isola', async function (req, res) {
   const lokasi = req.body.lokasi;
@@ -42,19 +33,41 @@ const isola = new Isola({
   //res.json(data);
 })
 
-/* 
-router.post('/isola', function (req, res) {
-  res.send('adding isolation case');
+router.put('/isola/:id', async function (req, res) {
+  console.log(req.params)
+  const isolaId = req.params.id
+  console.log(isolaId)
+  const isola = await Isola.findOne({ _id: isolaId })
+  if(!isola) return res.sendStatus(404)
+
+  const isola = new Isola({
+    lokasi: lokasi,
+    masuk: masuk,
+    keluar: keluar,
+    status: status
+  });
+  await isola.save()
+  res.send('Successfully update isolation case');
 })
 
-router.put('/isola/:id', function (req, res) {
-  res.send('update isolation case');
+
+/**
+ *  1. Get the id of the entry that the front end wants to update
+    2. Find the entry in the database, given the id
+    3. Get the value of what the front end wants the entry to be updated to
+    4. Update the entry with that value
+    We’ve done similar things to 1 and 2 in DELETE route, and 3 in POST route.
+ */
+
+router.delete('/isola/:id', async function (req, res) {
+  const isolaId = req.params.id
+  const isola = await Isola.findOne({ _id: isolaId })
+  if(!isola) return res.sendStatus(404)
+  await Isola.deleteOne({ _id: isolaId });
+  res.send('Successfully delete isolation case');
 })
 
-router.delete('/isola/:id', function (req, res) {
-   res.send('deletion isolation case');
-})
-*/
+
 app.use('/', router)
 
 app.listen(port, () => {
